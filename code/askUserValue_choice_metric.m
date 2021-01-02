730d6254ee93bdeb882cebb1f093440965dd9de3
maxvincent24 <maxime.vincent@philiaweb.com>
2021-01-02 17:34:25 +0100

    Part5 : Add of metric choice in rigid_registration_choice_metric.m file

---
+++ code/askUserValue_choice_metric.m
@@ -0,0 +1,27 @@
+function [tmin, tmax, tstep, rmin, rmax, rstep, metric] = askUserValue_choice_metric()
+    disp(['Enter the values of lower/upper bounds and steps for translation and rotation']);
+
+    prompt = 'tmin : ';
+    tmin = input(prompt);
+
+    prompt = 'tmax : ';
+    tmax = input(prompt);
+
+    prompt = 'tstep : ';
+    tstep = input(prompt);
+
+    prompt = 'rmin : ';
+    rmin = input(prompt);
+
+    prompt = 'rmax : ';
+    rmax = input(prompt);
+
+    prompt = 'rstep : ';
+    rstep = input(prompt);
+    
+    disp(['Choose the metric (enter 1 for Squared difference and 2 for Mutual information)']);
+    
+    prompt = 'metric : ';
+    metric = input(prompt);
+
+end
